c4a840cf1bc8b215a4a2462ab7f05f051f224ca9
gluman <gluman@yandex.ru>
2023-01-18 00:59:03 +0300

    Update Select.sql

--- Select.sql
+++ Select.sql
@@ -5,23 +5,23 @@
 
 
 --название и продолжительность самого длительного трека; +
---SELECT name_track, time_track  FROM tracks 
---WHERE time_track = (SELECT MAX(time_track) FROM tracks);
+SELECT name_track, time_track  FROM tracks 
+WHERE time_track = (SELECT MAX(time_track) FROM tracks);
 
 
 --название треков, продолжительность которых не менее 3,5 минуты;
---SELECT name_track FROM tracks
---WHERE time_track >= '00:03:30';
+SELECT name_track FROM tracks
+WHERE time_track >= '00:03:30';
 
 
 --названия сборников, вышедших в период с 2018 по 2020 год включительно;
---SELECT name_collection FROM collections
---WHERE year_collection BETWEEN '2018-01-01' AND '2020-12-31';
+SELECT name_collection FROM collections
+WHERE year_collection BETWEEN '2018-01-01' AND '2020-12-31';
 
 
 --исполнители, чье имя состоит из 1 слова;
---SELECT name_singer FROM singers
---WHERE split_part(name_singer, ' ', 2) = ''
+SELECT name_singer FROM singers
+WHERE split_part(name_singer, ' ', 2) = '';
 
 
 --название треков, которые содержат слово "мой"/"my".
